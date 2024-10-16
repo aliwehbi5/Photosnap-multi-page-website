b0b07fb779484173bce1951f9a77baaac7efca40
aliwehbi5 <ali_h_wehby@hotmail.com>
2024-10-16 12:40:07 +0300

    first commit

---
+++ src/components/betaComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="beta">
+    <div class="container">
+      <div class="row">
+        <h2 class="heading white">
+          We're in beta.<br />
+          Get your invite today!
+        </h2>
+        <router-link class="main-link" to="/"
+          >Get an invite
+          <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+        /></router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.beta {
+  position: relative;
+  background-image: url(https://abdulsamadayoade.github.io/Photosnap/images/bg-beta.jpg);
+  background-position: center;
+  background-size: cover;
+  padding: 50px 0;
+}
+.beta::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 6px;
+  background-image: linear-gradient(to bottom, #5a77ff, #bc7198, #ffc593);
+}
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+}
+h2.heading.white {
+  font-size: 3rem;
+  line-height: 3.5rem;
+  color: white;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  width: 50%;
+}
+@media (max-width: 991px) {
+  h2.heading.white {
+    font-size: 2.5rem;
+    line-height: 3rem;
+  }
+}
+@media (max-width: 768px) {
+  .row {
+    flex-direction: column;
+    align-items: start;
+    padding-left: 15px;
+  }
+  h2.heading.white {
+    font-size: 2rem;
+    line-height: 2.5rem;
+    width: auto;
+  }
+}
+</style>
---
+++ src/views/StoriesView.vue
@@ -0,0 +1,323 @@
+<script setup>
+import footerComponent from '@/components/footerComponent.vue'
+</script>
+<template>
+  <main>
+    <div class="landing-page">
+      <div class="container">
+        <div class="info">
+          <b>Last month's featured story </b>
+          <h1>Hazzy full moon of appalachia</h1>
+          <p class="date">March 2nd 2020 <span> by John Appleseed </span></p>
+          <p class="description">
+            The dissected plateau area, while not actually made up of geological mountains, is
+            popularly called "mountains," especially in eastern Kentucky and West Virginia, and
+            while the ridges are not high, the terrain is extremely rugged.
+          </p>
+          <router-link class="main-link" to="/"
+            >Get an invite
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+    </div>
+    <div class="stories">
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">The Mountains</h3>
+          <p class="artist">by John Appleseed</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">Sunset Cityscapes</h3>
+          <p class="artist">by Benjsamin Cruz</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">18 Days Voyage</h3>
+          <p class="artist">by Alexei Borodin</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">Architecturals</h3>
+          <p class="artist">by Samantha Brooke</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">The Mountains</h3>
+          <p class="artist">by John Appleseed</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">Sunset Cityscapes</h3>
+          <p class="artist">by Benjsamin Cruz</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">18 Days Voyage</h3>
+          <p class="artist">by Alexei Borodin</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">Architecturals</h3>
+          <p class="artist">by Samantha Brooke</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">The Mountains</h3>
+          <p class="artist">by John Appleseed</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">Sunset Cityscapes</h3>
+          <p class="artist">by Benjsamin Cruz</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">18 Days Voyage</h3>
+          <p class="artist">by Alexei Borodin</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">Architecturals</h3>
+          <p class="artist">by Samantha Brooke</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">The Mountains</h3>
+          <p class="artist">by John Appleseed</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">Sunset Cityscapes</h3>
+          <p class="artist">by Benjsamin Cruz</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">18 Days Voyage</h3>
+          <p class="artist">by Alexei Borodin</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+      <div class="story">
+        <div class="info">
+          <p class="date">March 16th 2020</p>
+          <h3 class="title">Architecturals</h3>
+          <p class="artist">by Samantha Brooke</p>
+          <span></span>
+          <router-link class="main-link" to="/"
+            >read story
+            <img src="https://abdulsamadayoade.github.io/Photosnap/images/arrow-white.svg" alt=""
+          /></router-link>
+        </div>
+      </div>
+    </div>
+    <footer-component></footer-component>
+  </main>
+</template>
+
+<style scoped>
+.landing-page {
+  background-image: url(https://abdulsamadayoade.github.io/Photosnap/images/moon-of-appalacia.jpg);
+  padding: 60px 0;
+  background-size: cover;
+  background-position: center;
+}
+.landing-page .info {
+  max-width: 400px;
+  color: white;
+}
+.landing-page b {
+  text-transform: uppercase;
+  letter-spacing: 2px;
+}
+.landing-page h1 {
+  text-transform: uppercase;
+  font-size: 3rem;
+  letter-spacing: 2px;
+  line-height: 4rem;
+  font-weight: bold;
+  margin: 25px 0;
+}
+.landing-page p.date {
+  color: #97a0a6;
+  font-size: 1rem;
+}
+.landing-page p.date span {
+  color: white;
+  font-size: 0.75rem;
+}
+.landing-page p.description {
+  color: #97a0a6;
+  margin: 25px 0;
+}
+@media (max-width: 991px) {
+  .landing-page h1 {
+    font-size: 2.5rem;
+    line-height: 3.5rem;
+  }
+}
+.story:nth-of-type(2) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/cityscapes.jpg);
+}
+.story:nth-of-type(3) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/18-days-voyage.jpg);
+}
+.story:nth-of-type(4) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/architecturals.jpg);
+}
+.story:nth-of-type(5) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/world-tour.jpg);
+}
+.story:nth-of-type(6) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/unforeseen-corners.jpg);
+}
+.story:nth-of-type(7) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/king-on-africa.jpg);
+}
+.story:nth-of-type(8) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/trip-to-nowhere.jpg);
+}
+.story:nth-of-type(9) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/rage-of-the-sea.jpg);
+}
+.story:nth-of-type(10) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/running-free.jpg);
+}
+.story:nth-of-type(11) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/behind-the-waves.jpg);
+}
+.story:nth-of-type(12) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/calm-waters.jpg);
+}
+.story:nth-of-type(13) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/milky-way.jpg);
+}
+.story:nth-of-type(14) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/dark-forest.jpg);
+}
+.story:nth-of-type(15) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/somwarpet.jpg);
+}
+.story:nth-of-type(16) {
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5)),
+    url(https://abdulsamadayoade.github.io/Photosnap/images/land-of-dreams.jpg);
+}
+</style>
